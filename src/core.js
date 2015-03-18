53d5914081e166927e29fd954fa939b029af1f93
Adam Mikulasev <adam@dealfetch.com.au>
2015-03-18 21:10:31 +1100

    add first, second & third result implementation (passes tests)

--- src/core.js
+++ src/core.js
@@ -9,3 +9,15 @@ function selection(bet) {
 function stake(bet) {
   return parseInt(bet.split(':')[3], 10);
 }
+
+function first(result) {
+  return result.split(':')[1]
+}
+
+function second(result) {
+  return result.split(':')[2]
+}
+
+function third(result) {
+  return result.split(':')[3]
+}
